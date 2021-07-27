372f9242a33edf83d7e948e91e7cb78ad1be84db
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-27 09:50:45 -0300

    Update set function in the class Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,11 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Liosvaldo"
     var cpf: String = "000.000.000-00"
+    private set
 }
 
 fun main(){
     val liosvaldo = Pessoa()
-
     println(liosvaldo.nome)
     println(liosvaldo.cpf)
 }
