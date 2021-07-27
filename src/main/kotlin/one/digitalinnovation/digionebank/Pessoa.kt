b65f284c20147b0bf626d437a6c04d4c12870b08
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-27 10:06:13 -0300

    Update constructor test and the function pessoaInfo

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome: String = "Liosvaldo"
     var cpf: String = "000.000.000-00"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome $cpf"
 }
 
 fun main(){
     val liosvaldo = Pessoa()
-    println(liosvaldo.nome)
-    println(liosvaldo.cpf)
+    println(liosvaldo.pessoaInfo())
 }
