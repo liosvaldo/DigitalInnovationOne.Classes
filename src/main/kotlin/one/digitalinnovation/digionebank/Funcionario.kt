38d03a0892a0613105a83f4c5d60bff70af2fb15
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-28 10:52:56 -0300

    Refactory the Funcionary class for abstract class.

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -5,8 +5,17 @@ import java.math.BigDecimal
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal
+    val salario: Double
 
 ): Pessoa(nome, cpf)  {
-    abstract fun calculoAuxilio()
+    protected abstract fun calculoAuxilio(): Double
+
+    override fun toString(): String =
+        """
+            Nome: $nome
+            cpf: $cpf
+            salario: $salario
+            auxilio: ${calculoAuxilio()}
+        """.trimIndent()
+
 }
