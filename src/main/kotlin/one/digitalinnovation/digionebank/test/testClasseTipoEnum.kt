2cb8df05991c249e4d9eb8965ac6570556d60542
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-28 08:59:00 -0300

    Upgrade testClasseTipoEnum

--- src/main/kotlin/one/digitalinnovation/digionebank/test/testClasseTipoEnum.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/test/testClasseTipoEnum.kt
@@ -4,7 +4,7 @@ import one.digitalinnovation.digionebank.ClienteTipo
 
 fun  main(){
     ClienteTipo.values().iterator().forEach {
-        println("${it.name} $it")
+        println("${it.name} ${it.descricao}")
     }
 
 }
