721b6fba98e8d0b6e5018b2ef54a48cc17deccf2
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-28 10:55:59 -0300

    Created Gerente class and testGerente file

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/test/testGerente.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.test
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main(){
+    val maria = Gerente("maria do carmo","000.555.444.02", 5000.00)
+    imprimeRelatorioFuncionario.imprime(maria)
+}
