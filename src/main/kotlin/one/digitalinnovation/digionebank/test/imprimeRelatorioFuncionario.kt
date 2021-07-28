bcf4b1bf9a140f5c596b0a9cf55c2ffcab0948db
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-28 10:53:30 -0300

    Created imprimeRelatorioFuncionario

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/test/imprimeRelatorioFuncionario.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.test
+
+import one.digitalinnovation.digionebank.Funcionario
+
+class imprimeRelatorioFuncionario {
+    companion object {
+        fun imprime(funcionario: Funcionario){println(funcionario.toString())}
+    }
+}
