39184e27dfeab98885912b0636d00683034182e4
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-07-28 10:55:01 -0300

    Created the class Analist and testAnalista

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/test/testAnalista.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.test
+
+import one.digitalinnovation.digionebank.Analista
+
+
+fun main(){
+    val liosvaldo = Analista("Liosvaldo", "000-000-000.00", 2000.00)
+    imprimeRelatorioFuncionario.imprime(liosvaldo)
+}
